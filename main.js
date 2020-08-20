559e0d5deff0c175f7a3c66631a0189be44dcbb5
Tia Kim <tiarekim@gmail.com>
2020-08-20 10:47:59 -0700

    added list of countries and cities

--- main.js
+++ main.js
@@ -31,6 +31,258 @@ const bikeTable = document.querySelector("table");
 const bikeTableBody = document.getElementById("bike-table-body");
 const exampleMapText = document.getElementById("example-map");
 const exploreBtn = document.querySelector(".explore-btn");
+const cityBtn = document.getElementById("city-list-btn");
+const countryBtn = document.getElementById("country-list-btn");
+const citySelect = document.getElementById("city-select");
+const countrySelect = document.getElementById("country-select");
+
+var countryShort = {
+  AF: "Afghanistan",
+  AX: "Aland Islands",
+  AL: "Albania",
+  DZ: "Algeria",
+  AS: "American Samoa",
+  AD: "Andorra",
+  AO: "Angola",
+  AI: "Anguilla",
+  AQ: "Antarctica",
+  AG: "Antigua And Barbuda",
+  AR: "Argentina",
+  AM: "Armenia",
+  AW: "Aruba",
+  AU: "Australia",
+  AT: "Austria",
+  AZ: "Azerbaijan",
+  BS: "Bahamas",
+  BH: "Bahrain",
+  BD: "Bangladesh",
+  BB: "Barbados",
+  BY: "Belarus",
+  BE: "Belgium",
+  BZ: "Belize",
+  BJ: "Benin",
+  BM: "Bermuda",
+  BT: "Bhutan",
+  BO: "Bolivia",
+  BA: "Bosnia And Herzegovina",
+  BW: "Botswana",
+  BV: "Bouvet Island",
+  BR: "Brazil",
+  IO: "British Indian Ocean Territory",
+  BN: "Brunei Darussalam",
+  BG: "Bulgaria",
+  BF: "Burkina Faso",
+  BI: "Burundi",
+  KH: "Cambodia",
+  CM: "Cameroon",
+  CA: "Canada",
+  CV: "Cape Verde",
+  KY: "Cayman Islands",
+  CF: "Central African Republic",
+  TD: "Chad",
+  CL: "Chile",
+  CN: "China",
+  CX: "Christmas Island",
+  CC: "Cocos (Keeling) Islands",
+  CO: "Colombia",
+  KM: "Comoros",
+  CG: "Congo",
+  CD: "Congo, Democratic Republic",
+  CK: "Cook Islands",
+  CR: "Costa Rica",
+  CI: "Cote D'Ivoire",
+  HR: "Croatia",
+  CU: "Cuba",
+  CY: "Cyprus",
+  CZ: "Czech Republic",
+  DK: "Denmark",
+  DJ: "Djibouti",
+  DM: "Dominica",
+  DO: "Dominican Republic",
+  EC: "Ecuador",
+  EG: "Egypt",
+  SV: "El Salvador",
+  GQ: "Equatorial Guinea",
+  ER: "Eritrea",
+  EE: "Estonia",
+  ET: "Ethiopia",
+  FK: "Falkland Islands (Malvinas)",
+  FO: "Faroe Islands",
+  FJ: "Fiji",
+  FI: "Finland",
+  FR: "France",
+  GF: "French Guiana",
+  PF: "French Polynesia",
+  TF: "French Southern Territories",
+  GA: "Gabon",
+  GM: "Gambia",
+  GE: "Georgia",
+  DE: "Germany",
+  GH: "Ghana",
+  GI: "Gibraltar",
+  GR: "Greece",
+  GL: "Greenland",
+  GD: "Grenada",
+  GP: "Guadeloupe",
+  GU: "Guam",
+  GT: "Guatemala",
+  GG: "Guernsey",
+  GN: "Guinea",
+  GW: "Guinea-Bissau",
+  GY: "Guyana",
+  HT: "Haiti",
+  HM: "Heard Island & Mcdonald Islands",
+  VA: "Holy See (Vatican City State)",
+  HN: "Honduras",
+  HK: "Hong Kong",
+  HU: "Hungary",
+  IS: "Iceland",
+  IN: "India",
+  ID: "Indonesia",
+  IR: "Iran, Islamic Republic Of",
+  IQ: "Iraq",
+  IE: "Ireland",
+  IM: "Isle Of Man",
+  IL: "Israel",
+  IT: "Italy",
+  JM: "Jamaica",
+  JP: "Japan",
+  JE: "Jersey",
+  JO: "Jordan",
+  KZ: "Kazakhstan",
+  KE: "Kenya",
+  KI: "Kiribati",
+  KR: "Korea",
+  KW: "Kuwait",
+  KG: "Kyrgyzstan",
+  LA: "Lao People's Democratic Republic",
+  LV: "Latvia",
+  LB: "Lebanon",
+  LS: "Lesotho",
+  LR: "Liberia",
+  LY: "Libyan Arab Jamahiriya",
+  LI: "Liechtenstein",
+  LT: "Lithuania",
+  LU: "Luxembourg",
+  MO: "Macao",
+  MK: "Macedonia",
+  MG: "Madagascar",
+  MW: "Malawi",
+  MY: "Malaysia",
+  MV: "Maldives",
+  ML: "Mali",
+  MT: "Malta",
+  MH: "Marshall Islands",
+  MQ: "Martinique",
+  MR: "Mauritania",
+  MU: "Mauritius",
+  YT: "Mayotte",
+  MX: "Mexico",
+  FM: "Micronesia, Federated States Of",
+  MD: "Moldova",
+  MC: "Monaco",
+  MN: "Mongolia",
+  ME: "Montenegro",
+  MS: "Montserrat",
+  MA: "Morocco",
+  MZ: "Mozambique",
+  MM: "Myanmar",
+  NA: "Namibia",
+  NR: "Nauru",
+  NP: "Nepal",
+  NL: "Netherlands",
+  AN: "Netherlands Antilles",
+  NC: "New Caledonia",
+  NZ: "New Zealand",
+  NI: "Nicaragua",
+  NE: "Niger",
+  NG: "Nigeria",
+  NU: "Niue",
+  NF: "Norfolk Island",
+  MP: "Northern Mariana Islands",
+  NO: "Norway",
+  OM: "Oman",
+  PK: "Pakistan",
+  PW: "Palau",
+  PS: "Palestinian Territory, Occupied",
+  PA: "Panama",
+  PG: "Papua New Guinea",
+  PY: "Paraguay",
+  PE: "Peru",
+  PH: "Philippines",
+  PN: "Pitcairn",
+  PL: "Poland",
+  PT: "Portugal",
+  PR: "Puerto Rico",
+  QA: "Qatar",
+  RE: "Reunion",
+  RO: "Romania",
+  RU: "Russian Federation",
+  RW: "Rwanda",
+  BL: "Saint Barthelemy",
+  SH: "Saint Helena",
+  KN: "Saint Kitts And Nevis",
+  LC: "Saint Lucia",
+  MF: "Saint Martin",
+  PM: "Saint Pierre And Miquelon",
+  VC: "Saint Vincent And Grenadines",
+  WS: "Samoa",
+  SM: "San Marino",
+  ST: "Sao Tome And Principe",
+  SA: "Saudi Arabia",
+  SN: "Senegal",
+  RS: "Serbia",
+  SC: "Seychelles",
+  SL: "Sierra Leone",
+  SG: "Singapore",
+  SK: "Slovakia",
+  SI: "Slovenia",
+  SB: "Solomon Islands",
+  SO: "Somalia",
+  ZA: "South Africa",
+  GS: "South Georgia And Sandwich Isl.",
+  ES: "Spain",
+  LK: "Sri Lanka",
+  SD: "Sudan",
+  SR: "Suriname",
+  SJ: "Svalbard And Jan Mayen",
+  SZ: "Swaziland",
+  SE: "Sweden",
+  CH: "Switzerland",
+  SY: "Syrian Arab Republic",
+  TW: "Taiwan",
+  TJ: "Tajikistan",
+  TZ: "Tanzania",
+  TH: "Thailand",
+  TL: "Timor-Leste",
+  TG: "Togo",
+  TK: "Tokelau",
+  TO: "Tonga",
+  TT: "Trinidad And Tobago",
+  TN: "Tunisia",
+  TR: "Turkey",
+  TM: "Turkmenistan",
+  TC: "Turks And Caicos Islands",
+  TV: "Tuvalu",
+  UG: "Uganda",
+  UA: "Ukraine",
+  AE: "United Arab Emirates",
+  GB: "United Kingdom",
+  US: "United States",
+  UM: "United States Outlying Islands",
+  UY: "Uruguay",
+  UZ: "Uzbekistan",
+  VU: "Vanuatu",
+  VE: "Venezuela",
+  VN: "Viet Nam",
+  VG: "Virgin Islands, British",
+  VI: "Virgin Islands, U.S.",
+  WF: "Wallis And Futuna",
+  EH: "Western Sahara",
+  YE: "Yemen",
+  ZM: "Zambia",
+  ZW: "Zimbabwe",
+};
 
 // list of country codes
 const countries = {
@@ -290,11 +542,14 @@ function handleSubmit(event) {
   const formData = new FormData(event.target);
   const country = formData.get("country");
   const city = formData.get("city");
+
   console.log(country, city);
 
   const countryCode = countries[country];
 
   removeTable();
+  removeNav(document.getElementById("nav"));
+  console.log(country, countryCode, city);
   getBike(country, countryCode, city);
 }
 
@@ -312,12 +567,26 @@ function initMap() {
   });
 }
 
+var worldData = [];
+var city = [];
+var country = [];
 // get SF Bike Stations as dafault : initial plan was to get stations all over the world by default but that is too much network requests.
 function getAllBike() {
   $.ajax({
     method: "GET",
     url: "http://api.citybik.es/v2/networks",
     success: function (data) {
+      worldData = data;
+      for (var i = 0; i < worldData.networks.length; i++) {
+        city.push(worldData.networks[i].location.city);
+        country.push(worldData.networks[i].location.country);
+      }
+
+      city = [...new Set(city)];
+      country = [...new Set(country)];
+
+      createCitySelectTags(city);
+      createCountrySelectTags(country);
       for (var i = 0; i < data.networks.length; i++) {
         if (data.networks[i].location.city === "San Francisco Bay Area, CA") {
           $.ajax({
@@ -350,14 +619,40 @@ function getAllBike() {
   });
 }
 
+function createCitySelectTags(cityArr) {
+  cityArr = cityArr.sort();
+  for (var i = 0; i < cityArr.length; i++) {
+    var option = document.createElement("option");
+    option.value = cityArr[i];
+    option.textContent = cityArr[i];
+    citySelect.appendChild(option);
+  }
+}
+
+function createCountrySelectTags(countryArr) {
+  for (var i = 0; i < countryArr.length; i++) {
+    countryArr[i] = countryShort[countryArr[i]];
+  }
+  countryArr = countryArr.sort();
+  for (var i = 0; i < countryArr.length; i++) {
+    var option = document.createElement("option");
+    option.value = countryArr[i];
+    option.textContent = countryArr[i];
+    countrySelect.appendChild(option);
+  }
+}
+
+var totalStations = 0;
 function getBike(country, countryCode, city) {
   $.ajax({
     method: "GET",
     url: "http://api.citybik.es/v2/networks",
     success: function (data) {
+      totalStations = 0;
       bikeTable.classList.remove("d-none");
       exampleMapText.classList.add("d-none");
       const regionData = [];
+      // let totalStations = 0;
 
       if (!city) {
         city = "";
@@ -416,6 +711,9 @@ function getBike(country, countryCode, city) {
               titleSub.textContent = "Bike stations in " + country;
             }
 
+            totalStations += data.network.stations.length;
+            console.log("total stations:", totalStations);
+            displayTotal(totalStations);
             for (let j = 0; j < data.network.stations.length; j++) {
               const marker = new google.maps.Marker({
                 position: {
@@ -449,10 +747,6 @@ function getBike(country, countryCode, city) {
                 })(content)
               );
 
-              // marker.addListener("click", () => {
-              //   infowindow.open(map, marker);
-              // });
-
               marker.setMap(map);
 
               // fill in the table in DOM
@@ -475,18 +769,33 @@ function getBike(country, countryCode, city) {
               );
             }
             map.setZoom(5);
+            pageTable();
           },
           error: function (err) {
             console.log(err);
           },
         });
       }
+
       exploreBtn.textContent = "Explore Another Location";
     },
     error: function (err) {
       console.log(err);
     },
   });
+}
+
+function displayTotal(totalStations) {
+  document.getElementById("stats").textContent =
+    "There are total " + totalStations + " Bike Stations in this area";
+}
+
+function removeNav(nav) {
+  console.log("hello from nav");
+  console.log(nav);
+  if (nav) {
+    nav.remove();
+  }
 }
 
 function removeTable() {
@@ -512,6 +821,13 @@ function addToTable(
   const cityTd = document.createElement("td");
   const countryTd = document.createElement("td");
 
+  stationTd.classList.add("station");
+  totalBikesTd.classList.add("total-bikes");
+  availableBikesTd.classList.add("available-bikes");
+  eBikesTd.classList.add("e-bikes");
+  cityTd.classList.add("city");
+  countryTd.classList.add("country");
+
   stationTd.textContent = stationName;
   totalBikesTd.textContent = totalBikes;
   availableBikesTd.textContent = availableBikes;
@@ -528,4 +844,33 @@ function addToTable(
     eBikesTd
   );
   bikeTableBody.appendChild(newRow);
+}
+
+function pageTable() {
+  $(document).ready(function () {
+    $("#data").after('<div id="nav"></div>');
+    var rowsShown = 10;
+    var rowsTotal = $("#data tbody tr").length;
+    var numPages = rowsTotal / rowsShown;
+    for (i = 0; i < numPages; i++) {
+      var pageNum = i + 1;
+      $("#nav").append('<a href="#" rel="' + i + '">' + pageNum + "</a> ");
+    }
+    $("#data tbody tr").hide();
+    $("#data tbody tr").slice(0, rowsShown).show();
+    $("#nav a:first").addClass("active");
+    $("#nav a").bind("click", function () {
+      $("#nav a").removeClass("active");
+      $(this).addClass("active");
+      var currPage = $(this).attr("rel");
+      var startItem = currPage * rowsShown;
+      var endItem = startItem + rowsShown;
+      $("#data tbody tr")
+        .css("opacity", "0.0")
+        .hide()
+        .slice(startItem, endItem)
+        .css("display", "table-row")
+        .animate({ opacity: 1 }, 300);
+    });
+  });
 }
